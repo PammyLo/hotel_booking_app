59e8bb7ed014641354c8e3666724ae247bc123ab
CallumGrindle <callumgrindle@gmail.com>
2019-10-23 12:06:56 +0100

    SHOW route working

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const parser = require ('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const cors = require ('cors')
+const cors = require ('cors');
 const createRouter = require('./helpers/create_router.js');
 
 app.use(cors());
@@ -11,5 +11,11 @@ app.use(parser.json());
 MongoClient.connect('mongodb://localhost:27017')
   .then(client => {
     const db = client.db('hotel_bookings');
-    const bookingsCollection = db.collection('bookings')
-  })
+    const bookingsCollection = db.collection('bookings');
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use('/api/bookings', bookingsRouter);
+  });
+
+app.listen(3000, function() {
+  console.log(`listening on port ${this.address().port}`);
+});
